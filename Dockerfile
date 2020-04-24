7b53faa9f4ea35bcf9b1ace5acdc7cca2a957061
Mark Blakeney <mark.blakeney@bullet-systems.net>
2020-04-24 13:25:45 +1000

    Update docker image

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,13 @@
-FROM python:3.7-alpine as base
+FROM python:3.8-alpine as base
 LABEL name=corsproxy maintainer="mark.blakeney@bullet-systems.net"
 
 FROM base as builder
-RUN apk add --no-cache build-base
+RUN apk add --no-cache build-base libffi-dev
 RUN mkdir /install
 WORKDIR /install
 
 COPY requirements.txt /
-RUN pip install --install-option="--prefix=/install" -r /requirements.txt
+RUN pip install --prefix=/install -r /requirements.txt
 
 FROM base
 COPY --from=builder /install /usr/local/
--- docker.sh
+++ docker.sh
@@ -2,34 +2,10 @@
 # Script to build docker image.
 # M.Blakeney, Oct 2019.
 
-usage() {
-    echo "Usage: $(basename $0) [-options]"
-    echo "Options:"
-    echo "-k (use new build kit)"
-    echo "-b <extra build args like --no-cache, --pull, etc>"
-    exit 1
-}
-
-DOCKER_BUILDKIT=0
-BUILDARGS=""
-while getopts kb: c; do
-    case $c in
-    k) DOCKER_BUILDKIT=1;;
-    b) BUILDARGS="$OPTARG";;
-    ?) usage;;
-    esac
-done
-
-shift $((OPTIND - 1))
-
-if [[ $# -ne 0 ]]; then
-    usage
-fi
-
 ARM=""
 if uname -m | grep -q arm; then
     ARM="-arm"
 fi
 
-export DOCKER_BUILDKIT
-docker build -t bulletmark/corsproxy$ARM . $BUILDARGS
+export DOCKER_BUILDKIT=1
+exec docker build -t bulletmark/corsproxy$ARM:latest . "$@"
